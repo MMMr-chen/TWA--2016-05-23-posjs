faf589a17ed5a0b47c3b3e7a841ef73e9943d85f
chen zhihao <18328576554@163.com>
2016-05-24 17:33:45 +0800

    reactor parseItemList

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-    var outputArray=[];
-    for(var i=0;i<inputs.length;i++){
-        if(judgeExist(inputs[i],outputArray)){
-            var item= {
-            barcode:inputs[i].barcode,
-            name : inputs[i].name,
-            number : 1,
-            unit : inputs[i].unit,
-            price : inputs[i].price,
-            totalprice : inputs[i].price
-            };
-            outputArray.push(item);
-        }
-
-    }
+    var outputArray = parseItemList(inputs);
     printData(outputArray);
+}
+function parseItemList(inputs){
+    var outputArray=[];
+        for(var i=0;i<inputs.length;i++){
+            if(judgeExist(inputs[i],outputArray)){
+                var item= {
+                barcode:inputs[i].barcode,
+                name : inputs[i].name,
+                number : 1,
+                unit : inputs[i].unit,
+                price : inputs[i].price,
+                totalprice : inputs[i].price
+                };
+                outputArray.push(item);
+            }
+        }
+        return outputArray;
 }
 function judgeExist(input,outputArray){
     for(var i=0;i<outputArray.length;i++){
@@ -32,7 +35,7 @@ function printData(outputArray){
     var allprice=0;
     for(var k=0;k<outputArray.length;k++){
         var out=outputArray[k];
-        var allprice=allprice+out.totalprice;
+        allprice += out.totalprice;
         itemString += '名称：'+out.name+'，数量：'+out.number+out.unit+'，单价：'+out.price.toFixed(2)+'(元)，小计：'+
         out.totalprice.toFixed(2)+'(元)'+'\n';
     }
